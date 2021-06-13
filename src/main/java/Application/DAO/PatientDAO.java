c0f7f15a8464385bb1b61bef37d0219b195d46aa
VanyaShestakov <vanka1108@gmail.com>
2021-06-13 17:09:25 +0300

    Added Patient DAO

---
+++ src/main/java/Application/DAO/PatientDAO.java
@@ -0,0 +1,10 @@
+package Application.DAO;
+
+import Application.Entity.Patient;
+
+import java.util.List;
+
+public interface PatientDAO {
+
+    public List<Patient> getAll();
+}
---
+++ src/main/java/Application/DAO/PatientDaoImpl.java
@@ -0,0 +1,24 @@
+package Application.DAO;
+
+import Application.Entity.Patient;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Repository
+public class PatientDaoImpl implements PatientDAO{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Patient> getAll() {
+        return sessionFactory.getCurrentSession().createQuery("from Patient", Patient.class).getResultList();
+    }
+
+}
